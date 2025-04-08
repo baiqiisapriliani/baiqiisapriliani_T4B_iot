9a501ba7713e8e2bbc5a79b32183aa6cfe1d94f2
baiqiisapriliani <baiqiisapriliani@gmail.com>
2025-04-08 15:28:21 +0700

    Menyelesaikan tugas praktik bab 14

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,84 @@
 #include <Arduino.h>
-int lampu=26;
-int lampu2=33;
-int lampu3=25;
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include "DHT.h"
+
+#define DHTPIN 27    
+#define DHTTYPE DHT22  
+DHT dht(DHTPIN, DHTTYPE);
+
+const char* ssid = "rindaii";
+const char* password = "hellostranger";
+
+unsigned long previousMillis = 0;
+const long interval = 5000;
+
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(115200);
-  Serial.println("Hello, ESP32!");
-  pinMode(lampu, OUTPUT);
-  pinMode(lampu2, OUTPUT);
-  pinMode(lampu3, OUTPUT);
+  delay(1000);  // kasih waktu buat serial nyala
+
+  WiFi.begin(ssid, password);
+  Serial.print("Menghubungkan ke WiFi");
+
+  int retry = 0;
+  while (WiFi.status() != WL_CONNECTED && retry < 20) {
+    delay(500);
+    Serial.print(".");
+    retry++;
+  }
+
+  if (WiFi.status() == WL_CONNECTED) {
+    Serial.println(" Terhubung!");
+    Serial.print("IP ESP32: ");
+    Serial.println(WiFi.localIP());
+  } else {
+    Serial.println(" Gagal terhubung ke WiFi.");
+  }
+
+  dht.begin();
 }
+
 void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(lampu, HIGH);
-  digitalWrite(lampu2, LOW);
-  digitalWrite(lampu3, LOW);
-  delay(1000);   // this speeds up the simulation
+  unsigned long currentMillis = millis();
 
-  digitalWrite(lampu, LOW);
-  digitalWrite(lampu2, HIGH);
-  digitalWrite(lampu3, LOW);
-  delay(1000);   // this speeds up the simulation
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
 
-  digitalWrite(lampu, LOW);
-  digitalWrite(lampu2, LOW);
-  digitalWrite(lampu3, HIGH);
-  delay(1000);
+    float h = round(dht.readHumidity());
+    float t = round(dht.readTemperature());
 
+    if (isnan(h) || isnan(t)) {
+      Serial.println("Gagal membaca dari sensor DHT!");
+      return;
+    }
+
+    float hic = dht.computeHeatIndex(t, h, false);
+
+    if (WiFi.status() == WL_CONNECTED) {
+      HTTPClient http;
+      String url = "http://f1c4-139-195-134-207.ngrok-free.app/api/posts";
+      http.begin(url);
+      http.addHeader("Content-Type", "application/json");
+
+      String payload = "{\"nama_sensor\":\"Sensor GD\", \"nilai1\":" + String(h) + ", \"nilai2\":" + String(t) + "}";
+      Serial.println("Payload: " + payload);
+
+      int httpResponseCode = http.POST(payload);
+
+      Serial.print("Kode respons HTTP: ");
+      Serial.println(httpResponseCode);
+
+      if (httpResponseCode == 200 || httpResponseCode == 201) {
+        String response = http.getString();
+        Serial.println("Respons dari server:");
+        Serial.println(response);
+      } else {
+        Serial.println("Gagal mengirim data");
+      }
+
+      http.end();
+    } else {
+      Serial.println("WiFi tidak terhubung, tidak mengirim data.");
+    }
+  }
 }
-
-
